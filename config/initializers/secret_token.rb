d38da5e05be6ff4ad8108c1ba1f24bc2b5e00d91
Peter <cpeteryu@gmail.com>
2014-03-02 00:10:10 -0500

    Starting ecommerce template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StoreEcommerce::Application.config.secret_key_base = '73fba8ad10454232524faee5c9f1e7506de357da91a0e7c90b47eb8740798874f133a1808543bda51504a055e5d188e60efdaf026b5e4e07485383b0f27022fc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StoreEcommerce::Application.config.session_store :cookie_store, key: '_store-ecommerce_session'
